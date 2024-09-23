cf6c164d592beac75945d962744b793776eb4fc0
bgdcc <150472288+bgdcc@users.noreply.github.com>
2024-09-23 23:30:16 +0200

    Add files via upload

--- Trucker4.java
+++ Trucker4.java
@@ -1,44 +1,64 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
 
 class Trucker4 {
+    // Runs the program.
     public void run() {
         // Initialize the number of mileages.
         int n = 0;
 
+        // Initialize the main ArrayLists.
         ArrayList<Integer> mileages = new ArrayList<>();
         ArrayList<Integer> locations = new ArrayList<>();
+
+        // Get input for the variables.
         setup(n, mileages, locations);
 
+        /*  Initialize a HashMap which record the availability of the element at index i.
+        *   All values evaluate to true at first, as all the values are available.
+        */
         HashMap<Integer, Boolean> isItAvailable = new HashMap<>();
 
         for (int i = 0; i < mileages.size(); i++) {
             isItAvailable.put(i, true);
         }
-        HashSet<Integer> locations2 = new HashSet<>();
+
+        // Create a HashSet for "locations" to facilitate a less complex searching algorithm.
+        HashSet<Integer> locationSet = new HashSet<>();
 
         for (int k: locations) {
-            locations2.add(k);
+            locationSet.add(k);
         }
 
+        // Initialize an ArrayList which will store the list of solutions.
         ArrayList<Integer> solutions = new ArrayList<>();
 
+        // Initialize the variable which will store the current location.
         int currentLocation = 0;
 
-        
+        ArrayList<Integer> finalArr = solve(mileages, locations, locationSet, 
+                                   solutions, isItAvailable, currentLocation);
 
-        for (int s: solve(mileages, locations2, solutions, isItAvailable, currentLocation)) {
-            System.out.print(s + " ");
+        // Print out the elements.
+        for (int i = 0; i < finalArr.size(); i++) {
+            if (i == finalArr.size() - 1) {
+                System.out.print(finalArr.get(i));
+            } else {
+                System.out.print(finalArr.get(i) + " ");
+            }
         }
+        
+        
     }
 
+    // Get input for the program.
     private void setup(int n, ArrayList<Integer> mileages, ArrayList<Integer> locations) {
         Scanner scan = new Scanner(System.in);
 
+        // Initialize the number of mileages.
         n = scan.nextInt();
 
         // Add all the mileages to the ArrayList.
@@ -56,11 +76,28 @@ class Trucker4 {
     }
 
 
-    private ArrayList<Integer> solve(ArrayList<Integer> mileages, HashSet<Integer> locations2, 
-                                     ArrayList<Integer> solutions, HashMap<Integer, Boolean> isItAvailable, 
+    /* Solves the problem.
+
+     * The program checks the availability of the input through a loop.
+     * The element is added to the "solutions" ArrayList if it is available,
+     * then it calls itself again.
+     * 
+     * If there is no possible element which can be added without matching an 
+     * element in "locations", the method ends and goes back to the previous call,
+     * removing the previously added element in the process.
+     * 
+     * If at any point "solutions" has the necessary number of elements, the method will
+     * return this result.
+     */
+    private ArrayList<Integer> solve(ArrayList<Integer> mileages,
+                                     ArrayList<Integer> locations, 
+                                     HashSet<Integer> locationSet, 
+                                     ArrayList<Integer> solutions, 
+                                     HashMap<Integer, Boolean> isItAvailable, 
                                      int currentLocation) {
 
-        if (solutions.size() == mileages.size() && currentLocation > Collections.max(locations2)) {
+        // If the ArrayList has a complete list of solutions, return it.
+        if (solutions.size() == mileages.size()) {
             return solutions;
         }
         
@@ -69,31 +106,34 @@ class Trucker4 {
             int possibleLocation = currentLocation + mileages.get(i);
 
             // Checks if element at index i is available to use.
-            if (isItAvailable.get(i) && !locations2.contains(possibleLocation)) {
+            if (isItAvailable.get(i) && !locationSet.contains(possibleLocation)) {
 
                 /* If "locations" does not contain it,
                  * "currentLocation", "isItAvailable" and "solutions" are all updated.
-                 * 
-                 * 
                 */
                 currentLocation += mileages.get(i);
                 solutions.add(i);
                 isItAvailable.put(i, false);
 
                 // The function calls itself recursively.
-                ArrayList<Integer> result = solve(mileages, locations2, solutions, 
+                ArrayList<Integer> result = solve(mileages, locations, locationSet, solutions, 
                                                   isItAvailable, currentLocation);
 
-                if (result.size() == mileages.size() && currentLocation > Collections.max(locations2)) {
+                // Return result if, after the call, the result has enough elements.
+                if (result.size() == mileages.size()) {
                     return result;
                 }
                 
+                // Update the variables if the element at index i is not valid.
                 isItAvailable.put(i, true);
                 solutions.remove(solutions.size() - 1);
                 currentLocation -= mileages.get(i);
             }
         }
 
+        /* Return nothing in case there is no solution.
+         * This return method will not be reached when the input is optimal.
+        */
         return new ArrayList<>();
     }
 
